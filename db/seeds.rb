5cd83c21c095feb7c25815f5970b15abc774f454
Benard Abwao <benard.okoth.abwao@gmail.com>
2022-10-20 13:12:04 +0300

    Initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+Bird.create!(name: 'Black-Capped Chickadee', species: 'Poecile Atricapillus')
+Bird.create!(name: 'Grackle', species: 'Quiscalus Quiscula')
+Bird.create!(name: 'Common Starling', species: 'Sturnus Vulgaris')
+Bird.create!(name: 'Mourning Dove', species: 'Zenaida Macroura')
