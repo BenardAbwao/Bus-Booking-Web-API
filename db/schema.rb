10b2caaa0df7935993d4fb71a694c78161ad3a71
Benard Abwao <benard.okoth.abwao@gmail.com>
2022-10-20 20:38:14 +0300

    Added drivers data

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_20_135653) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "birds", force: :cascade do |t|
+    t.string "name"
+    t.string "species"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "bookings", force: :cascade do |t|
     t.integer "bus_id"
     t.integer "customer_id"
