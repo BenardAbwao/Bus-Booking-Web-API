8eb22bc5b380c961c9fadb58d36f49032f2cb2c1
Bernardngari <bernardngari.ke@gmail.com>
2022-10-22 22:38:00 +0300

    Add create action to driver controller

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,14 +1,32 @@
 class DriversController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
     def index
         drivers = Driver.all
         render json: drivers 
     end
 
+    def create
+      if(params[:role]=== driver)
+        driver = Driver.create(driver_params)
+        render json: driver, status: :ok
+      else
+        return null
+      end
+    end
+
     def show
-        drivers = Driver.find(params[:id])
-        render json: drivers
-rescue ActiveRecord::RecordNotFound
-        render json: "Driver not found", status: :not_found
+        driver = Driver.find(params[:id])
+        render json: driver, status: :ok
+    end
+
+    private
+
+    def driver_params
+      params.permit(:name, :role)
+    end
+
+    def render_not_found_response
+      render json: {error: "Driver was not found"}, status: :not_found
     end
 
 end
