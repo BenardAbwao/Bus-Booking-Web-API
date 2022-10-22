e943ddba7acdb73c59c85c4d76a325ee21c3e918
Bernardngari <bernardngari.ke@gmail.com>
2022-10-22 13:08:02 +0300

    create a buses controller

---
+++ app/controllers/buses_controller.rb
@@ -0,0 +1,43 @@
+class BusesController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  def index
+    buses = Bus.all 
+    render json: buses, status: :ok
+  end
+
+  def show
+    bus = find_bus
+    render json: bus, status: :ok
+  end
+
+  def create
+    bus = Bus.create(bus_params)
+    render json: bus, status: :created 
+  end
+
+  def destroy
+    bus = find_bus
+    bus.destroy
+    head :no_content
+  end
+
+  private
+
+  def render_not_found_response
+    render json: {error: "Bus not found"}, status: :not_found
+  end
+
+  def bus_params
+    params.permit(:plate_number, :no_of_seats, :cost_per_seat, :from, :to, 
+    :time_of_travel, :available)
+  end
+
+  def render_unprocessable_entity_response(exception)
+    render json: {errors: exception.record.errors.full_messages}, status: :unprocessable_entity
+  end
+
+  def find_bus
+    render json: Bus.find(params[:id]), status: :ok
+  end
+end
