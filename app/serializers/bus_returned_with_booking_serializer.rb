e4a5a9906a8e13b899d728f99503624adf2ca2ad
Benard Abwao <benard.okoth.abwao@gmail.com>
2022-11-01 18:57:13 +0300

    fixed comma error

--- app/serializers/bus_returned_with_booking_serializer.rb
+++ app/serializers/bus_returned_with_booking_serializer.rb
@@ -1,3 +1,3 @@
 class BusReturnedWithBookingSerializer < ActiveModel::Serializer
-  attributes :plate_number, :cost_per_seat, :from, :to, :travel_date, :travel_time, 
+  attributes :plate_number, :cost_per_seat, :from, :to, :travel_date, :travel_time
 end
