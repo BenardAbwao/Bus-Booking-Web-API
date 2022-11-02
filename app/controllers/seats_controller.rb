35c7d588266d075a4ee10e2f801be92bf328f95f
Bernardngari <bernardngari.ke@gmail.com>
2022-11-02 09:22:32 +0300

    sort seats by seat no

--- app/controllers/seats_controller.rb
+++ app/controllers/seats_controller.rb
@@ -2,7 +2,7 @@ class SeatsController < ApplicationController
   #before_action :authorize_potential_customer, only: [:update]
   def index
     seats = Seat.page(page).per(per_page)
-    render json: seats.order(created_at: :asc), status: :ok
+    render json: seats.order("seat_no ASC"), status: :ok
   end
 
   def show
@@ -21,7 +21,7 @@ class SeatsController < ApplicationController
     #customer_id..using the customer in session
     #seat_no
     seat.update(seat_params)
-    render json: seat.order(created_at: :asc), status: :ok
+    render json: seat.order("seat_no ASC"), status: :ok
   end
 
   private
