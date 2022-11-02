2c854dca90aa7449d1f6e23cfa0f2177a692806b
Bernardngari <bernardngari.ke@gmail.com>
2022-11-02 23:50:07 +0300

    Add error handling in customer controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,16 +1,14 @@
 class CustomersController < ApplicationController
+rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
   def index
     customers = Customer.page(page).per(per_page)
     render json: customers, status: :ok
   end
   
   def create
-    if(params[:role]=== "customer")
-      customer = Customer.create(customer_params)
+      customer = Customer.create!(customer_params)
       render json: customer, status: :ok
-    else
-      return null
-    end
   end
   
   def update
@@ -27,6 +25,14 @@ class CustomersController < ApplicationController
   private
   def find_customer
     Customer.find(params[:id])
+  end
+
+  def render_unprocessable_entity_response(exception)
+    render json: {errors: exception.record.errors.full_messages}, status: :unprocessable_entity
+  end
+
+  def render_not_found_response
+    return render json: {error: "Customer not found"}, status: :not_found
   end
 
   def customer_params
